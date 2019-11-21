1eb2d76111a12f794ad02102fc554617d84d189a
zero428 <1483404039@qq.com>
2019-11-21 23:17:19 +0800

    work10

--- work7/index.js
+++ work7/index.js
@@ -1,5 +1,4 @@
 function sort(method){
-<<<<<<< HEAD
     let origin = document.getElementById('origin').value
     let logs
     console.log(method)
@@ -10,19 +9,4 @@ function sort(method){
     }
     showDiv = document.getElementById('Anim')
     showDiv.innerHTML = logs
-=======
-    //读取待排序数据
-    let origin = document.getElementById('origin').value
-    let logs
-    console.log(method)
-    
-    if(method=='bubble'){
-        logs = bubbleSort(origin)
-    }else
-        if(method=='insert'){
-            logs = insertSort(origin)
-        }
-        showDiv = document.getElementById('Anim')
-        showDiv.innerHTML = logs
->>>>>>> 799447371f88073cdd8481716ab182fbac117951
 }
--- work7/utils.js
+++ work7/utils.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function bubbleSort(str) {
     let arr = str.split(',')
     for (let i = 0; i <arr.length -1; i++) {
@@ -19,27 +18,6 @@ function insertSort(str) {
             if (arr[j - 1] >arr[j]) {
                 [arr[j - 1], arr[j]] = [arr[j],arr[j - 1]]
             }
-=======
-function bubbleSort(str){
-    let arr = str.split(',')
-    for(let i = 0;i<arr.length - 1; i++){
-        for(let j=0; j<arr.length- i - 1;j++){
-
-            if( arr [ j ] > arr [ j + 1 ] ){
-                [arr [ j ],arr [ j + 1 ] ] = [ arr [ j + 1 ], arr [ j ] ]
-            }
-        }
-    }
-    return arr
-}
-function insertSort(str){
-    let arr = str.split(',')
-    for(let i = 1; i<arr.length ; i++){
-        for (let j = i; j>0; j--){
-                if(arr[j-1]>arr[j]){
-                    [arr[j-1],arr[j] ]= [arr[j],arr[j-1 ] ]
-                }
->>>>>>> 799447371f88073cdd8481716ab182fbac117951
         }
     }
     return arr
