e4c4cad57cd29e152f869bb4f10ae1c8a3c870f1
zero428 <1483404039@qq.com>
2019-12-16 13:14:28 +0800

    work16

--- work16/index.js
+++ work16/index.js
@@ -1,32 +1,33 @@
-function $(id) {
+function $(id){
     return document.getElementById(id);
 }
-$('smallBox').onmouseover = function () {
+
+$('smallBox').onmouseover = function(){
     $('mask').style.display = 'block';
     $('bigBox').style.display = 'block';
 };
+
 $('smallBox').onmouseout = function () {
-    $('mask').style.diaplay = 'none';
+    $('mask').style.display = 'none';
     $('bigBox').style.display = 'none';
-};
-$('smallBox').onmousemove = function (event) {
+}
+
+$('smallBox').onmousemove = function (){
     var event = event || window.event;
     var pageX = event.pageX || event.clientX + document.documentElement.scrollLeft;
     var pageY = event.pageY || event.clientY + document.documentElement.scrollTop;
-    var boxX = event.pageX - $('box').offsetLeft;
-    var boxY = event.pageY - $('box').offsetTop;
+    var boxX = pageX - $('box').offsetLeft;
+    var boxY = pageY - $('box').offsetTop;
     var maskX = boxX - $('mask').offsetWidth / 2;
-    var maskY = boxY - $('mask').offsetHeight / 2;
-    if (maskX < 0) {
-        maskX = 0;
-    }
-    if (maskX > $('smallBox').offsetWidth - $('mask').offsetWidth) {
+    var maskY = boxY - $('mask').offsetHeight / 2 ;
+
+    if (maskX > $('smallBox').offsetWidth - $('mask').offsetWidth){
         maskX = $('smallBox').offsetWidth - $('mask').offsetWidth;
     }
-    if (maskY < 0) {
+    if(maskY < 0){
         maskY = 0;
     }
-    if (maskY > $('smallBox').offsetHeight - $('mask').offsetHeight) {
+    if(maskY > $('smallBox').offsetHeight - $('mask').offsetHeight){
         maskY = $('smallBox').offsetHeight - $('mask').offsetHeight;
     }
     $('mask').style.left = maskX + 'px';
@@ -36,4 +37,4 @@ $('smallBox').onmousemove = function (event) {
     var rate = bigImgToMove / maskToMove;
     $('bigImg').style.left = -rate * maskX + 'px';
     $('bigImg').style.top = -rate * maskY + 'px';
-}
+};
