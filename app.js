b772dc47c46af3460cfe496b974a5ea83c996993
zero428 <1483404039@qq.com>
2019-12-30 16:26:20 +0800

    work19

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ const bodyParser = require("body-parser")
 app.use(express.static('.'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use (express.static('../uploads'))
+const multer = require('multer');
+var storage = multer.diskStorage({
+    //设置上传后文件路径，会自动创建一个uploasd目录，与jswork同级目录。
+    destination: function (req,file, cb){
+        cb(null, '../uploads')
+    },
+    //给上传文件重命名，获取添加后缀名
+    filename: function (req, file, cb){
+        var fileFormat = (file.originalname).split(".");
+        cb(null, file.fieldname+'-'+Date.now() + "." + fileFormat[fileFormat.length - 1]);
+    }
+});
+let upload = multer({ storage: storage })
+//单文件上传获取信息
+app.post('/upload',upload.single('file'),function(req,res,next){
+    var file=req.file;
+    console.log("original file name is "+file.originalname);
+    console.log("file name is " + file.filename);
+    res.json('/'+file.filename);//这行代码必须要有，否则Browser会处于wait状态
+})
 app.post('/formBuilder',function(req,res){
     console.log(req.body)
     res.send(req.body)
@@ -53,3 +74,31 @@ const openDefaultBrower=function(url){
     }
 }
 openDefaultBrower('http://localhost:8080')
+var ws = require("nodejs-websocket")
+let id=0
+
+var server = ws.createServer(function (conn){
+    id++
+    conn.name = "p"+id
+    broadcast(server,'有新人加入.')
+    conn.on("text", function (str){
+        if(str.slice(0,9)=='nickname|'){
+            conn.name=str.split('|')[1]
+            broadcast(server,conn.name+'上线了。')
+            return
+        }
+        broadcast(server,conn.name+':'+str)
+    })
+    conn.on('connect',function(){
+        conn.name = "name"
+    })
+    conn.on("close", function (code, reason) {
+        console.log("Connecton closed")
+    })
+}).listen(8081,()=>console.log('socket server listening on:8081'))
+
+function broadcast(server, msg){
+    server.connections.forEach(function (conn){
+        conn.sendText(msg)
+    })
+}
